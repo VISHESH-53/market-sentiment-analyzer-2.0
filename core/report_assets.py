155afa51fa1491c96549bf9f28c7a9c874e5d914
VISHESH AGRAWAL <visheshagrawal53@gmail.com>
2026-01-29 18:08:46 +0530

    Update report_assets.py

--- core/report_assets.py
+++ core/report_assets.py
@@ -11,3 +11,15 @@ def save_equity_curve(bt_df, filename="equity_curve.png"):
     plt.tight_layout()
     plt.savefig(filename)
     plt.close()
+def save_pie_chart(labels, values, title, filename):
+    plt.figure(figsize=(4, 4))
+    plt.pie(
+        values,
+        labels=labels,
+        autopct="%1.1f%%",
+        startangle=90
+    )
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
